d925b1850609249f53959c58fa8fe31ffd2fde1a
AntoineDia <diambu.antoine@gmail.com>
2019-03-21 11:35:50 +0100

    working environement

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <title>Exe Loops</title>
+  <meta name="description" content=" ça clique ">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <link rel="shortcut icon" href="ass/img/favicon.ico">
+  <link rel="stylesheet" href="ass/css/normalise.css">
+  <link rel="stylesheet" href="ass/css/style.css">
+</head>
+<body>
+
+</body>
+</html>
